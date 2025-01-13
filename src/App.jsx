02c2bc006575843433b4ced795503fcf1b54ab9b
Vanina <55039948+VauroraS@users.noreply.github.com>
2025-01-13 14:09:25 -0300

    Setup inicial: React y Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <p className="text-xl font-bold text-green-600">Hello Tailwind!</p>
+    </div>
+  );
+}
+
+export default App;
+
